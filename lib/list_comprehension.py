7f4415a94c72cd57dbfe88e3bdf0327c6e42d2e2
Peter Njiru <peternjirum@gmail.com>
2023-07-09 20:49:31 -0400

    completes list comprehension lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed = [n + "!" for n in sentence_list]
+    return exclaimed
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
